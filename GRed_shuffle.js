cefc9574eee90de7bb0ceb06de16d0db5e22cedb
sonicstride <sonicstride.tw@gmail.com>
2024-02-12 14:02:37 +0800

    GRed_shuffle

--- GRed_shuffle.js
+++ GRed_shuffle.js
@@ -1,28 +1,51 @@
 
+let pass_type = ['█', '█', '▢'];
+
+class GRed_shuffle {
 
 
-let pass_type = '█';
-
-class GRed_shuffle{
-
-            
-    constructor(HTMLElement){
-        this.elemant =  HTMLElement;
+    constructor(HTMLElement) {
+        this.elemant = HTMLElement;
         this.origin_text = this.elemant.innerHTML.split("");
+        this.new_text = this.elemant.innerHTML.split("");
     }
-            
-    display(i){
-        this.origin_text[i] = pass_type;
+
+    fade_in(i) {
+        this.new_text[i] = pass_type[Math.floor(Math.random() * pass_type.length)];
         this.elemant.innerHTML = "";
-        for(let i=0; i<this.origin_text.length; i++){
-            this.elemant.innerHTML += this.origin_text[i];
-        }      
+        for (let i = 0; i < this.new_text.length; i++) {
+            this.elemant.innerHTML += this.new_text[i];
+        }
+
+    }
+
+    fade_out(i) {
+        this.new_text[i] = this.origin_text[i];
+        this.elemant.innerHTML = "";
+        for (let i = 0; i < this.new_text.length; i++) {
+            this.elemant.innerHTML += this.new_text[i];
+        }
+    }
+
+    delay(flag) {
+        if (flag == 1) {
+            for (let i = 0; i < this.origin_text.length; i++) {
+                this.timeout = setTimeout(() => this.fade_in(i), i * 30);
+            }
+        }
+        else if (flag == 0) {
+            for (let i = 0; i < this.origin_text.length; i++) {
+                this.timeout = setTimeout(() => this.fade_out(i), i * 30);
+            }
+        }
     }
 
 
-    start(){
-        for(let i=0; i<this.origin_text.length; i++){
-            this.timeout = setTimeout(() => this.display(i), i*50);
-        }
+    start(delay = 0) {
+        setTimeout(() => this.delay(1), delay);
+    }
+
+    stop(delay = 0) {
+        setTimeout(() => this.delay(0), delay);
     }
 }
